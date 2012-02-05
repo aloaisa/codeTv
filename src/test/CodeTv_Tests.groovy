7b3b619edcd8e2b44c0c415737b47b2536db155c
Alvaro Garcia Loaisa <loaisa@gmail.com>
2012-02-05 21:06:52 +0100

    Third Test Fail.

--- src/test/CodeTv_Tests.groovy
+++ src/test/CodeTv_Tests.groovy
@@ -7,12 +7,19 @@ class CodeTv_Tests  extends GroovyTestCase {
 
     void testReturnAVideoLinkById_1() {
 
-        assert new VideoFinder().getVideoLinkById(1) == "http://vimeo.com/9028476" : "Error to Obtain a StringCalculator Video on Vimeo."
+        assert new VideoFinder().getVideoLinkById(1) == "http://vimeo.com/9028476" : "Error to Obtain a " +
+				"StringCalculator Video on Vimeo."
     }
 	
 	void testReturnAVideoLinkById_2() {
 		
-		assert new VideoFinder().getVideoLinkById(2) == "http://groovy.org.es/home/story/324" : "Error to Obtain a \"Seminario de introducción a Groovy\" Video."
+		assert new VideoFinder().getVideoLinkById(2) == "http://groovy.org.es/home/story/324" : "Error to " +
+				"Obtain a \"Seminario de introducción a Groovy\" Video."
 	}
 		
+	void testReturnAVideoLinkById_3() {
+		
+		assert new VideoFinder().getVideoLinkById(3) == "http://vimeo.com/11931804" : "Error to Obtain a " +
+				"\"Groovy y la productividad para desarrolladores Java\" Video."
+	}
 }
