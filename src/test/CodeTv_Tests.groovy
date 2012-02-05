f11ae5f6bb499df5e14f62e4dc301e058d86c53e
Alvaro Garcia Loaisa <loaisa@gmail.com>
2012-02-05 20:27:08 +0100

    Second Test

--- src/test/CodeTv_Tests.groovy
+++ src/test/CodeTv_Tests.groovy
@@ -5,8 +5,14 @@ import main.VideoFinder
 
 class CodeTv_Tests  extends GroovyTestCase {
 
-    void testReturnAVideoLinkById() {
-        
+    void testReturnAVideoLinkById_1() {
+
         assert new VideoFinder().getVideoLinkById(1) == "http://vimeo.com/9028476" : "Obtain a StringCalculator Video on Vimeo."
     }
+	
+	void testReturnAVideoLinkById_2() {
+		
+		assert new VideoFinder().getVideoLinkById(2) == "http://groovy.org.es/home/story/324" : "Obtain a \"Seminario de introducción a Groovy\" Video."
+	}
+		
 }
