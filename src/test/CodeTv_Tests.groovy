0072f225dc80eed4e27751b19619e5fc10dd4524
Alvaro Garcia Loaisa <loaisa@gmail.com>
2012-02-05 20:35:33 +0100

    Second Test Ok.

--- src/test/CodeTv_Tests.groovy
+++ src/test/CodeTv_Tests.groovy
@@ -7,12 +7,12 @@ class CodeTv_Tests  extends GroovyTestCase {
 
     void testReturnAVideoLinkById_1() {
 
-        assert new VideoFinder().getVideoLinkById(1) == "http://vimeo.com/9028476" : "Obtain a StringCalculator Video on Vimeo."
+        assert new VideoFinder().getVideoLinkById(1) == "http://vimeo.com/9028476" : "Error to Obtain a StringCalculator Video on Vimeo."
     }
 	
 	void testReturnAVideoLinkById_2() {
 		
-		assert new VideoFinder().getVideoLinkById(2) == "http://groovy.org.es/home/story/324" : "Obtain a \"Seminario de introducción a Groovy\" Video."
+		assert new VideoFinder().getVideoLinkById(2) == "http://groovy.org.es/home/story/324" : "Error to Obtain a \"Seminario de introducción a Groovy\" Video."
 	}
 		
 }
