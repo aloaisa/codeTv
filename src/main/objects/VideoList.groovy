3b2d5cf5a50e08429a9e2f8f383b7ec00a10a4de
Alvaro Garcia Loaisa <loaisa@gmail.com>
2012-03-30 21:25:14 +0200

    Start with Spock testing.

--- src/main/objects/VideoList.groovy
+++ src/main/objects/VideoList.groovy
@@ -3,50 +3,71 @@ package main.objects
 import org.codehaus.groovy.ast.stmt.ThrowStatement;
 
 class VideoList {
-
-	 private static List<Video> videoList = null
-	 private static VideoList INSTANCE = null
-	 
-	 private videoList() {}
-	 
-	 def static VideoList getInstance() {
-		 
-		 if (INSTANCE == null) {
-			 INSTANCE = new VideoList()
-			 initialize()
-		 }
-		 
-		 return INSTANCE
+	
+	private static VideoList INSTANCE = null
+	private List<Video> listOfVideos
+	
+	public static VideoList getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new VideoList()
+		}
+		return INSTANCE
 	 }
 
-	private static initialize() {
-		videoList = new ArrayList<Video>()
+	protected VideoList() {
+	
+		listOfVideos = new ArrayList<Video>()
+		
+		// Initialize Videos
+		Video video1 = new Video()
+		video1.setId(1)
+		video1.setDescription("StringCalculator Groovy")
+		video1.setLink("http://vimeo.com/9028476")
+			
+		Video video2 = new Video()
+		video2.setId(2)
+		video2.setDescription("Seminario de introducción a Groovy")
+		video2.setLink("http://groovy.org.es/home/story/324")
+			
+		Video video3 = new Video()
+		video3.setId(3)
+		video3.setDescription("Groovy y la productividad para desarrolladores Java")
+		video3.setLink("http://vimeo.com/11931804")
+			
+		addVideo(video1)
+		addVideo(video2)
+		addVideo(video3)
 	}
 	
-	def static addVideo(Video video) {
+	
+	def addVideo(Video video) {
 		
-		Video findIt = findVideoById(video)
+		Video findIt = findVideoById(video.getId())
 		
 		if (findIt == null) {
-			videoList.add(video)
+			listOfVideos.add(video)
 		} else {
 			 throw new Exception("This Id Exists.") 
 		}
 	}
 	
-	def static Video findVideoById(Integer id) {
+	def Video findVideoById(Integer id) {
 		Video result = null
 		
-		videoList.each { item ->
-			if (item.id.equals(id)) {
+		listOfVideos.each { item ->
+			if (item.id == id) {
 				result = item
 			}
 		}
 		
 		return result
 	}
+	
+	def deleteAllVideos() {
 		
-	def static getVideoLinkByDescriptionContains() {
+	}
+	
+	def getVideoLinkByDescriptionContains() {
 		return
 	}
 }
